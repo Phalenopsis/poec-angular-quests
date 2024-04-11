8779640aa9e7c94f057186449000710c471d8537
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-11 11:33:15 +0200

    feat: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'user', component: UserProfileComponent },
+  { path: '', component: UserProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
