10eb0325ca475b9f970b2e97367137f92bacb9a2
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-11 10:39:13 +0200

    feat: add menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
