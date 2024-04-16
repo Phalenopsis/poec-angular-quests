45676ce2acb38ace17cd32de4518b74d053e674c
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-16 12:04:45 +0200

    feat: user kitten component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateKittenComponent } from './create-kitten/create-kitten.component';
 import { ListKittenComponent } from './list-kitten/list-kitten.component';
+import { UserKittenComponent } from './user-kitten/user-kitten.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateKittenComponent,
-    ListKittenComponent
+    ListKittenComponent,
+    UserKittenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-kitten/user-kitten.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-kitten',
+  templateUrl: './user-kitten.component.html',
+  styleUrl: './user-kitten.component.scss'
+})
+export class UserKittenComponent {
+
+}
