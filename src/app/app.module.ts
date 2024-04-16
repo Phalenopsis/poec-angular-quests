b076db7e41e44ec322c06f865290117a7247d666
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-16 12:03:53 +0200

    feat: list kitten component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateKittenComponent } from './create-kitten/create-kitten.component';
+import { ListKittenComponent } from './list-kitten/list-kitten.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateKittenComponent
+    CreateKittenComponent,
+    ListKittenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/list-kitten/list-kitten.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-kitten',
+  templateUrl: './list-kitten.component.html',
+  styleUrl: './list-kitten.component.scss'
+})
+export class ListKittenComponent {
+
+}
