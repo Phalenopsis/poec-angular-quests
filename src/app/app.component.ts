2667857c7932f349b1c7a48c0c109fd785d7796b
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-16 13:28:07 +0200

    feat: kitten created received by app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Kitten } from './models/types/kitten.type';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quest-angular';
+  kittenCreated: Kitten = {
+    name: "",
+    race: "",
+    birthday: new Date(),
+    imageSrc: ""
+  };
+  isKittenCreated: boolean = false;
+
+  onReceiveKitten(event: Kitten) {
+    this.isKittenCreated = true;
+    this.kittenCreated = event;
+  }
 }
