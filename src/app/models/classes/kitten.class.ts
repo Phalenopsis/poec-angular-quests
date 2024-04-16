8381cb856eb9ba325cffbb2fc040949a927d5f3c
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-16 13:56:31 +0200

    refactor: change kitten type to class

---
+++ src/app/models/classes/kitten.class.ts
@@ -0,0 +1,9 @@
+export class Kitten {
+    constructor(
+        public name: string = "",
+        public race: string = "",
+        public birthday: Date = new Date(),
+        public imageSrc: string = ""
+
+    ) { }
+}
