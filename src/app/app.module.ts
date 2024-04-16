58acd214bd22a796d6420accc8cba77d0a9586fd
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-16 10:07:37 +0200

    feat: add createonomatopia component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateOnomatopiaComponent } from './create-onomatopia/create-onomatopia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateOnomatopiaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/create-onomatopia/create-onomatopia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-onomatopia',
+  templateUrl: './create-onomatopia.component.html',
+  styleUrl: './create-onomatopia.component.scss'
+})
+export class CreateOnomatopiaComponent {
+
+}
