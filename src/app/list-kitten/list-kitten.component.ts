3a26ad341bde8890456e7fac5923f19815538762
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-16 21:22:31 +0200

    feat: kitten mock

--- src/app/list-kitten/list-kitten.component.ts
+++ src/app/list-kitten/list-kitten.component.ts
@@ -7,7 +7,11 @@ import { Kitten } from '../models/classes/kitten.class';
   styleUrl: './list-kitten.component.scss'
 })
 export class ListKittenComponent {
-  kittenList: Kitten[] = [];
+  kittenList: Kitten[] = [
+    new Kitten("Toulouse", "aristocat", new Date("1905-12-25"), "https://vainkeurz.com/wp-content/uploads/2021/06/toulouse.png"),
+    new Kitten("Marie", "aristocat", new Date("1905-12-25"), "https://i.pinimg.com/236x/56/7e/a6/567ea64edf03cc158cd3ac7954e55c98.jpg"),
+    new Kitten("Berlioz", "aristocat", new Date("1905-12-25"), "https://vainkeurz.com/wp-content/uploads/2021/06/berlioz.png")
+  ];
   private _newKitten: Kitten = new Kitten();
 
   @Input() set newKitten(value: Kitten) {
