42c9f7c2e6bd0c665899e706a58a70e30adc2fcd
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-18 14:12:43 +0200

    refactor: add type to constant

--- src/app/models/constant.ts
+++ src/app/models/constant.ts
@@ -1,4 +1,4 @@
-export const NUMBER_OF_HEARTS = 10;
-export const NUMBER_OF_HEART_TICKS = 40;
-export const TICK_DURATION = 60;
-export const BASE_HEART_VELOCITY = 15;
+export const NUMBER_OF_HEARTS: number = 10;
+export const NUMBER_OF_HEART_TICKS: number = 40;
+export const TICK_DURATION: number = 60;
+export const BASE_HEART_VELOCITY: number = 15;
