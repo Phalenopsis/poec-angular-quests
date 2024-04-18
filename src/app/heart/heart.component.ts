952f254b373538d549d8cefe83745203b2fb01a5
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-18 13:48:13 +0200

    refactor: erase unused imports

--- src/app/heart/heart.component.ts
+++ src/app/heart/heart.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Position } from '../models/classes/position.class';
 
 
