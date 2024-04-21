d72821a9566c5c5770cf175fafe4472c5028d1e5
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-21 19:55:36 +0200

    feat: add account module using nmm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+	{ path: 'account', loadChildren: () =>  import('./modules/account/account.module').then(m  => m.AccountModule) },
 	{ path: 'admin', loadChildren: () =>  import('./modules/admin/admin.module').then(m  => m.AdminModule) },
 	];
 
